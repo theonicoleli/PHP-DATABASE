d449a5d230fadf1ace12f5cfec3fd1a3b3dbc8fa
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-10-10 15:01:05 -0300

    PHP & DATABASE PROJECT

--- project/src/application/Running.php
+++ project/src/application/Running.php
@@ -3,7 +3,6 @@ include(__DIR__ . '/../entities/Pessoa.php');
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form input values
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $rendimento = $_POST["rendimento"];
@@ -11,18 +10,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pensaoAlimenticia = $_POST["pensaoAlimenticia"];
 
     try {
-        // Inclua o arquivo da classe Pessoa
-
-        // Create a new Pessoa object
         $pessoa = new Pessoa($nome, $email, $rendimento, $dependentes, $pensaoAlimenticia);
 
-        // Add the Pessoa to the database
         $pessoa->addPessoa();
 
-        // Call the function to display the list after adding a person
         $pessoa->verLista();
     } catch (\Exception $e) {
-        // Handle any exceptions, e.g., display an error message
         echo "Error: " . $e->getMessage();
     }
 }
